c7a45958b066c46adcfd9daa6169b993358e1714
alejandro juarez <alejandrosebastian.juarez@mercadolibre.com>
2024-07-19 17:13:03 -0300

    add tests for limited service

--- app/src/test/java/com/alejua/ratelimitednotificator/mocks/service/MockedNotificationServiceImpl.java
+++ app/src/test/java/com/alejua/ratelimitednotificator/mocks/service/MockedNotificationServiceImpl.java
@@ -1,19 +1,39 @@
 package com.alejua.ratelimitednotificator.mocks.service;
 
-
 import com.alejua.ratelimitednotificator.domain.NotificationType;
 import com.alejua.ratelimitednotificator.service.NotificationService;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MockedNotificationServiceImpl implements NotificationService {
 
     private Long calledSendTimes = 0L;
 
+    private final Map<String, Long> calledByTypeUser = new HashMap<>();
+
     @Override
     public void send(NotificationType type, String userId, String message) {
-        calledSendTimes++;
+        addCalledToMemory(userId, type);
     }
 
     public Long getCalledSendTimes() {
         return calledSendTimes;
+    }
+
+    public Long getCalledByTypeAndUser(String userId, NotificationType type) {
+        return calledByTypeUser.get(createKey(userId, type));
+    }
+
+    private void addCalledToMemory(String userId, NotificationType type) {
+        calledSendTimes++;
+
+        String key = createKey(userId, type);
+        Long calledNotificationByTypeUser = calledByTypeUser.getOrDefault(key, 0L);
+        calledNotificationByTypeUser++;
+        calledByTypeUser.put(key, calledNotificationByTypeUser);
+    }
+
+    private String createKey(String userId, NotificationType type) {
+        return userId + "-" + type;
     }
 }
--- app/src/test/java/com/alejua/ratelimitednotificator/service/RateLimitedNotificationServiceTest.java
+++ app/src/test/java/com/alejua/ratelimitednotificator/service/RateLimitedNotificationServiceTest.java
@@ -1,6 +1,9 @@
 package com.alejua.ratelimitednotificator.service;
 
-import com.alejua.ratelimitednotificator.domain.NotificationType;
+import static com.alejua.ratelimitednotificator.domain.NotificationType.MARKETING;
+import static com.alejua.ratelimitednotificator.domain.NotificationType.NEWS;
+import static com.alejua.ratelimitednotificator.domain.NotificationType.UPDATE;
+
 import com.alejua.ratelimitednotificator.domain.RateLimitConfiguration;
 import com.alejua.ratelimitednotificator.domain.RateLimitConfigurationRule;
 import com.alejua.ratelimitednotificator.mocks.repository.MockedNotificationRepository;
@@ -11,6 +14,9 @@ import org.junit.jupiter.api.Test;
 
 class RateLimitedNotificationServiceTest {
 
+    public static final String USER_ID_1 = "user1";
+    public static final String USER_ID_2 = "user2";
+
     @Test
     public void sendMsgToNotLimitedTypeShouldSendAllMsg() {
         MockedNotificationServiceImpl notificationService = new MockedNotificationServiceImpl();
@@ -20,10 +26,11 @@ class RateLimitedNotificationServiceTest {
                 notificationService, notificationRepository, RateLimitConfiguration.WITHOUT_LIMIT);
 
         for (int i = 0; i < 100; i++) {
-            service.send(NotificationType.NEWS, "user", "news " + i);
+            service.send(NEWS, USER_ID_1, "news " + i);
         }
 
         Assertions.assertEquals(100, notificationService.getCalledSendTimes());
+        Assertions.assertEquals(100, notificationService.getCalledByTypeAndUser(USER_ID_1, NEWS));
         Assertions.assertEquals(0, notificationRepository.getCalledSaveNotificationTimes());
     }
 
@@ -34,7 +41,7 @@ class RateLimitedNotificationServiceTest {
 
         // 3 msg each 1 minute for marketing
         RateLimitConfiguration conf = new RateLimitConfiguration(Map.of(
-                NotificationType.MARKETING, new RateLimitConfigurationRule(3L, 60L)
+                MARKETING, new RateLimitConfigurationRule(3L, 60L)
         ));
 
         RateLimitedNotificationServiceImpl service = new RateLimitedNotificationServiceImpl(
@@ -42,10 +49,11 @@ class RateLimitedNotificationServiceTest {
 
         // but type is news, so for news is unlimited
         for (int i = 0; i < 100; i++) {
-            service.send(NotificationType.NEWS, "user", "news " + i);
+            service.send(NEWS, USER_ID_1, "news " + i);
         }
 
         Assertions.assertEquals(100, notificationService.getCalledSendTimes());
+        Assertions.assertEquals(100, notificationService.getCalledByTypeAndUser(USER_ID_1, NEWS));
         Assertions.assertEquals(0, notificationRepository.getCalledSaveNotificationTimes());
     }
 
@@ -56,16 +64,17 @@ class RateLimitedNotificationServiceTest {
 
         // 3 msg each 1 minute for NEWS
         RateLimitConfiguration conf = new RateLimitConfiguration(Map.of(
-                NotificationType.NEWS, new RateLimitConfigurationRule(3L, 60L)
+                NEWS, new RateLimitConfigurationRule(3L, 60L)
         ));
 
         RateLimitedNotificationServiceImpl service = new RateLimitedNotificationServiceImpl(
                 notificationService, notificationRepository, conf);
 
-        service.send(NotificationType.NEWS,"user","news 1");
-        service.send(NotificationType.NEWS,"user","news 2");
+        service.send(NEWS, USER_ID_1,"news 1");
+        service.send(NEWS, USER_ID_1,"news 2");
 
         Assertions.assertEquals(2, notificationService.getCalledSendTimes());
+        Assertions.assertEquals(2, notificationService.getCalledByTypeAndUser(USER_ID_1, NEWS));
         Assertions.assertEquals(2, notificationRepository.getCalledSaveNotificationTimes());
     }
 
@@ -76,17 +85,18 @@ class RateLimitedNotificationServiceTest {
 
         // 3 msg each 1 minute for NEWS
         RateLimitConfiguration conf = new RateLimitConfiguration(Map.of(
-                NotificationType.NEWS, new RateLimitConfigurationRule(3L, 60L)
+                NEWS, new RateLimitConfigurationRule(3L, 60L)
         ));
 
         RateLimitedNotificationServiceImpl service = new RateLimitedNotificationServiceImpl(
                 notificationService, notificationRepository, conf);
 
-        service.send(NotificationType.NEWS,"user","news 1");
-        service.send(NotificationType.NEWS,"user","news 2");
-        service.send(NotificationType.NEWS,"user","news 3");
+        service.send(NEWS, USER_ID_1,"news 1");
+        service.send(NEWS, USER_ID_1,"news 2");
+        service.send(NEWS, USER_ID_1,"news 3");
 
         Assertions.assertEquals(3, notificationService.getCalledSendTimes());
+        Assertions.assertEquals(3, notificationService.getCalledByTypeAndUser(USER_ID_1, NEWS));
         Assertions.assertEquals(3, notificationRepository.getCalledSaveNotificationTimes());
     }
 
@@ -97,21 +107,117 @@ class RateLimitedNotificationServiceTest {
 
         // 2 msg each 1 minute for marketing
         RateLimitConfiguration conf = new RateLimitConfiguration(Map.of(
-                NotificationType.NEWS, new RateLimitConfigurationRule(2L, 60L)
+                NEWS, new RateLimitConfigurationRule(2L, 60L)
         ));
 
         RateLimitedNotificationServiceImpl service = new RateLimitedNotificationServiceImpl(
                 notificationService, notificationRepository, conf);
 
-        service.send(NotificationType.NEWS,"user","news 1");
-        service.send(NotificationType.NEWS,"user","news 2");
-        service.send(NotificationType.NEWS,"user","news 3");
+        service.send(NEWS, USER_ID_1,"news 1");
+        service.send(NEWS, USER_ID_1,"news 2");
+        service.send(NEWS, USER_ID_1,"news 3");
 
         Assertions.assertEquals(2, notificationService.getCalledSendTimes());
+        Assertions.assertEquals(2, notificationService.getCalledByTypeAndUser(USER_ID_1, NEWS));
         Assertions.assertEquals(2, notificationRepository.getCalledSaveNotificationTimes());
     }
 
-    // TODO test with two or more types and combinations
-    // TODO test with two or more users and combinations
+    @Test
+    public void sendMsgToLimitedTypeShouldLimitMsgOnlyForLimitedType() {
+        MockedNotificationServiceImpl notificationService = new MockedNotificationServiceImpl();
+        MockedNotificationRepository notificationRepository = new MockedNotificationRepository();
 
+        // 2 msg each 1 minute for marketing
+        RateLimitConfiguration conf = new RateLimitConfiguration(Map.of(
+                NEWS, new RateLimitConfigurationRule(2L, 60L)
+        ));
+
+        RateLimitedNotificationServiceImpl service = new RateLimitedNotificationServiceImpl(
+                notificationService, notificationRepository, conf);
+
+        for (int i = 0; i < 100; i++) {
+            service.send(UPDATE, USER_ID_1, "update " + i);
+        }
+
+        service.send(NEWS, USER_ID_1,"news 1");
+        service.send(NEWS, USER_ID_1,"news 2");
+        service.send(NEWS, USER_ID_1,"news 3");
+
+        // 100 of update and 2 of news
+        Assertions.assertEquals(102, notificationService.getCalledSendTimes());
+        Assertions.assertEquals(100, notificationService.getCalledByTypeAndUser(USER_ID_1, UPDATE));
+        Assertions.assertEquals(2, notificationService.getCalledByTypeAndUser(USER_ID_1, NEWS));
+        Assertions.assertEquals(2, notificationRepository.getCalledSaveNotificationTimes());
+    }
+
+    @Test
+    public void sendMsgToLimitedTypeShouldAllowSendToMoreThanOneLimitedType() {
+        MockedNotificationServiceImpl notificationService = new MockedNotificationServiceImpl();
+        MockedNotificationRepository notificationRepository = new MockedNotificationRepository();
+
+        RateLimitConfiguration conf = new RateLimitConfiguration(Map.of(
+                UPDATE, new RateLimitConfigurationRule(1L, 60L),
+                NEWS, new RateLimitConfigurationRule(2L, 60L)
+        ));
+
+        RateLimitedNotificationServiceImpl service = new RateLimitedNotificationServiceImpl(
+                notificationService, notificationRepository, conf);
+
+        service.send(UPDATE, USER_ID_1, "update 1");
+        service.send(UPDATE, USER_ID_1, "update 2");
+
+        service.send(NEWS, USER_ID_1,"news 1");
+        service.send(NEWS, USER_ID_1,"news 2");
+        service.send(NEWS, USER_ID_1,"news 3");
+
+        service.send(MARKETING, USER_ID_1, "marketing 1");
+        service.send(MARKETING, USER_ID_1, "marketing 2");
+
+        Assertions.assertEquals(5, notificationService.getCalledSendTimes());
+        Assertions.assertEquals(1, notificationService.getCalledByTypeAndUser(USER_ID_1, UPDATE));
+        Assertions.assertEquals(2, notificationService.getCalledByTypeAndUser(USER_ID_1, MARKETING));
+        Assertions.assertEquals(2, notificationService.getCalledByTypeAndUser(USER_ID_1, NEWS));
+        Assertions.assertEquals(3, notificationRepository.getCalledSaveNotificationTimes());
+    }
+
+    @Test
+    public void sendMsgToLimitedTypeShouldBeOkWithDifferentTypesAndUsers() {
+        MockedNotificationServiceImpl notificationService = new MockedNotificationServiceImpl();
+        MockedNotificationRepository notificationRepository = new MockedNotificationRepository();
+
+        RateLimitConfiguration conf = new RateLimitConfiguration(Map.of(
+                UPDATE, new RateLimitConfigurationRule(1L, 60L),
+                NEWS, new RateLimitConfigurationRule(2L, 60L)
+        ));
+
+        RateLimitedNotificationServiceImpl service = new RateLimitedNotificationServiceImpl(
+                notificationService, notificationRepository, conf);
+
+        service.send(UPDATE, USER_ID_1, "update 1");
+        service.send(UPDATE, USER_ID_1, "update 2"); // limited
+
+        service.send(NEWS, USER_ID_1,"news 1");
+        service.send(NEWS, USER_ID_1,"news 2");
+        service.send(NEWS, USER_ID_1,"news 3"); // limited
+
+        service.send(MARKETING, USER_ID_1, "marketing 1");
+        service.send(MARKETING, USER_ID_1, "marketing 2");
+
+        service.send(UPDATE, USER_ID_2, "update 1");
+        service.send(UPDATE, USER_ID_2, "update 2"); // limited
+        service.send(MARKETING, USER_ID_2, "marketing 1");
+        service.send(NEWS, USER_ID_2,"news 1");
+
+        Assertions.assertEquals(8, notificationService.getCalledSendTimes());
+
+        Assertions.assertEquals(1, notificationService.getCalledByTypeAndUser(USER_ID_1, UPDATE));
+        Assertions.assertEquals(2, notificationService.getCalledByTypeAndUser(USER_ID_1, MARKETING));
+        Assertions.assertEquals(2, notificationService.getCalledByTypeAndUser(USER_ID_1, NEWS));
+
+        Assertions.assertEquals(1, notificationService.getCalledByTypeAndUser(USER_ID_2, UPDATE));
+        Assertions.assertEquals(1, notificationService.getCalledByTypeAndUser(USER_ID_2, MARKETING));
+        Assertions.assertEquals(1, notificationService.getCalledByTypeAndUser(USER_ID_2, NEWS));
+
+        Assertions.assertEquals(5, notificationRepository.getCalledSaveNotificationTimes());
+    }
 }
